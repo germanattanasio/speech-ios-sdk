6b182f4dc8f56f0ff533d8137f58e92b3a1e5641
Rob Smart <smartrob@uk.ibm.com>
2015-09-10 09:41:33 +0100

    updated license headers

--- watsonsdk/AuthConfiguration.m
+++ watsonsdk/AuthConfiguration.m
@@ -1,10 +1,18 @@
-//
-//  AuthConfiguration.m
-//  watsonsdk
-//
-//  Created by Daisuke Sato on 8/5/15.
-//  Copyright (c) 2015 IBM. All rights reserved.
-//
+/**
+ * Copyright IBM Corporation 2015
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
 
 #import "AuthConfiguration.h"
 
