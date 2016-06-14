c105d82ea0ce509e11261d29c7673f1c8f0a2d19
Guillermo Ignacio Enriquez Gutierrez <ignacio@jp.ibm.com>
2016-06-14 12:02:40 +0900

    Dynamic Framework 😊

---
+++ watsonspeech/watsonsdk.h
@@ -0,0 +1,29 @@
+//
+//  WatsonSDK.h
+//  WatsonSDK
+//
+//  Created by Ignacio on 6/13/16.
+//  Copyright © 2016 IBM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WatsonSDK.
+FOUNDATION_EXPORT double WatsonSDKVersionNumber;
+
+//! Project version string for WatsonSDK.
+FOUNDATION_EXPORT const unsigned char WatsonSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WatsonSDK/WatsonSDK.h>
+
+#import "AuthConfiguration.h"
+#import "TTSConfiguration.h"
+#import "OpusHelper.h"
+#import "SpeechUtility.h"
+#import "STTConfiguration.h"
+#import "TextToSpeech.h"
+#import "SpeechToText.h"
+#import "TTSCustomWord.h"
+#import "TTSCustomVoice.h"
+#import "WebSocketAudioStreamer.h"
+#import "ogg.h"
